784b84df0562b72209f225f814f0c3391878eda4
Enrique Green <47195508+BruceManuelwayne@users.noreply.github.com>
2021-03-24 22:06:26 -0300

    ejercicios3
    preguntar sobre dos letras de la consola.

---
+++ Ejercicio3/Ejercicio3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo cki;
+            Console.WriteLine("ingrese una tecla, x para terminar");
+            var input = Console.ReadKey();
+          
+            do
+            {
+                
+                cki = Console.ReadKey();
+                Console.WriteLine(cki.Key.ToString()); 
+            } while (cki.Key != ConsoleKey.X); 
+           
+            
+            //porque en la consola me muestra dos letras? 
+        }
+    }
+}
